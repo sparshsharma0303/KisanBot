53c0648e886e1298fd5515bb1e6ac379cd3f58da
sparsh sharma <sparshsharma0303@gmail.com>
2026-06-16 19:48:04 +0530

    add logger, exception, utils boilerplate to src

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,4 +2,8 @@ from datasets import load_dataset
 import pandas as pd
 
 def ingest_data(save_path:str =  "data/raw/kisanbot_corpus.csv"):
-    ds = load_dataset
+    ds = load_dataset("KisanVaani/agriculture-qa-english-only")
+    df = ds["train"].to_pandas()
+    df.dropna(inplace = True)
+    df.to_csv(save_path,index = False)
+    
